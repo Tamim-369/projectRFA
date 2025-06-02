0b853f3b0db2b90974f132ffca98cb07268b1f61
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-06-02 21:37:36 +0600

    fixed error in import

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from .methods.image import see, click
+from methods.image import see
 import pyautogui
 import time
-while True:
-        location = see('images/facebook.png')
-        shortLocation = see('images/short.png')
-        print(location)
-        if location is not None:
-           pyautogui.hotkey('ctrl', 'w')
-           time.sleep(2)
-        if shortLocation is not None:
-           pyautogui.hotkey('ctrl', 'w')
-           time.sleep(2)
-        else: 
-             print("Trying again...")
-    
+
+if __name__ == "__main__":
+   while True:
+           location = see('images/facebook.png')
+           shortLocation = see('images/short.png')
+           print(location)
+           if location is not None:
+              pyautogui.hotkey('ctrl', 'w')
+              time.sleep(2)
+           if shortLocation is not None:
+              pyautogui.hotkey('ctrl', 'w')
+              time.sleep(2)
+           else: 
+                print("Trying again...")
